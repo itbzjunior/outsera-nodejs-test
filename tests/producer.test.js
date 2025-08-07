8fe6a1e1fff4e4372eaeb60b83797de0507d9b24
Itamar Junior <web@agenciabike.com.br>
2025-08-07 17:04:57 -0300

    fix: Ajuste no teste dos produtores

--- tests/producer.test.js
+++ tests/producer.test.js
@@ -2,13 +2,11 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /producers/interval-awards', () => {
-  it('deve retornar produtores com os parametro "min" e "max" corretamente', async () => {
+
+  it('deve retornar produtores com os parametros "min" e "max" corretamente', async () => {
     
     // Faz a chamada
     const res = await request(app).get('/producers/interval-awards');
-
-    // Espera que o código de resposta seja 200 (OK)
-    expect(res.status).toBe(200);
 
     // Espera que o resultado tenta os parâmetros "min" e "max" no body
     expect(res.body).toHaveProperty('min');
@@ -17,19 +15,27 @@ describe('GET /producers/interval-awards', () => {
     // Espera que os parâmetros "min" e "max" sejam uma array
     expect(Array.isArray(res.body.min)).toBe(true);
     expect(Array.isArray(res.body.max)).toBe(true);
+  });
+
+  it('deve retornar os produtores de acordo com os dados enviados no CSV do teste', async () => {
+    
+    // Faz a chamada
+    const res = await request(app).get('/producers/interval-awards');
 
     // Verificação dos dados esperados no "min"
-    //const minProducer = res.body.min.find(p => p.producer === 'Joel Silver');
-    //expect(minProducer).toBeDefined();
-    //expect(minProducer.interval).toBe(1);
-    //expect(minProducer.previousWin).toBe(1990);
-    //expect(minProducer.followingWin).toBe(1991);
+    expect(res.body.min[0]).toEqual({
+      producer: 'Joel Silver',
+      interval: 1,
+      previousWin: 1990,
+      followingWin: 1991
+    });
 
     // Verificação dos dados esperados no "max"
-    //const maxProducer = res.body.max.find(p => p.producer === 'Matthew Vaughn');
-    //expect(maxProducer).toBeDefined();
-    //expect(maxProducer.interval).toBe(13);
-    //expect(maxProducer.previousWin).toBe(2002);
-    //expect(maxProducer.followingWin).toBe(2015);
+    expect(res.body.max[0]).toEqual({
+      producer: 'Matthew Vaughn',
+      interval: 13,
+      previousWin: 2002,
+      followingWin: 2015
+    });
   });
 });
